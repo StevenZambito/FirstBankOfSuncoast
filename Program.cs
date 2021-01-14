cf53f571de628ee5ab9cfef06b307b06a890e26b
StevenZambito <zambito.steven@icloud.com>
2021-01-14 13:06:17 -0500

    Initial commit: Created banner message and while loop.

--- Program.cs
+++ Program.cs
@@ -2,11 +2,51 @@
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void BannerMessage(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine("----------------------");
+            Console.WriteLine(message);
+            Console.WriteLine("----------------------");
+            Console.WriteLine();
+        }
+
+        static string PromptForString(string prompt)
+        {
+            Console.WriteLine(prompt);
+
+            var userInput = Console.ReadLine().ToUpper().Trim();
+            return userInput;
+        }
+        static void Main(string[] args)
+        {
+            var userHasChosenToQuit = false;
+
+            while (userHasChosenToQuit == false)
+            {
+                BannerMessage("First Bank of Suncoast");
+
+                Console.WriteLine();
+                Console.WriteLine("Menu:");
+                Console.WriteLine();
+                Console.WriteLine("Checking");
+                Console.WriteLine("Savings");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Quit");
+                Console.WriteLine();
+
+                var userResponse = PromptForString("Checking or Savings?");
+
+                if (userResponse == "QUIT")
+                {
+                    userHasChosenToQuit = true;
+                }
+            }
+
+            BannerMessage("Have a nice day!");
+        }
     }
-  }
 }
